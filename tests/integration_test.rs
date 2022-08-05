d0eb86b1cc0b5e29ec48a2b8bf14087da627383f
Sam Beam <sbeam@syxyz.net>
2022-08-05 16:01:32 -0400

    fix test with BufReader

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,15 +1,20 @@
 use bankster;
 use rust_decimal_macros::dec;
+use std::io::{BufReader};
+
+fn bufferize(s: &str) -> Box<BufReader<&[u8]>> {
+    Box::new(BufReader::new(s.as_bytes()))
+}
 
 #[test]
 fn test_basic_data() {
-    let data = "amount,client,tx,type
+    let data = bufferize("amount,client,tx,type
     100.01,  99, 1,deposit
     2.9095,  34,2, deposit  
     xxx,     xx
     9.99,    99,  3,deposit
     47.1,  34,7, deposit  
-    20,      34,5,withdrawal";
+    20,      34,5,withdrawal");
     let records = bankster::read_csv(data);
 
     assert_eq!(records.len(), 2);
@@ -19,14 +24,14 @@ fn test_basic_data() {
 
 #[test]
 fn test_disputed_and_resolved_data() {
-    let data = "type,client,tx,amount
+    let data = bufferize("type,client,tx,amount
     deposit,33,3,9.99
     deposit,33,4,47.1
     dispute,33,3,0,
     deposit,99,1,100.01
     deposit,99,2,2.9
     dispute,99,1,0
-    resolve,33,3,0";
+    resolve,33,3,0");
     let records = bankster::read_csv(data);
 
     assert_eq!(records.get(&99).unwrap().available, dec!(2.90));
@@ -37,14 +42,14 @@ fn test_disputed_and_resolved_data() {
 
 #[test]
 fn test_chargeback_data() {
-    let data = "type,client,tx,amount
+    let data = bufferize("type,client,tx,amount
     deposit,33,3,9.99
     deposit,33,4,47.1
     dispute,33,3,0
     deposit,99,1,100.01
     deposit,99,2,2.9
     chargeback,99,1,0
-    chargeback,33,3,0";
+    chargeback,33,3,0");
     let records = bankster::read_csv(data);
 
     // chargeback on 99 invalid because never disputed
