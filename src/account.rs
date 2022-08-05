7fea535ee758038c7d914f18b789b988961578c0
Sam Beam <sbeam@syxyz.net>
2022-08-05 11:15:40 -0400

    impl and test disputes

--- src/account.rs
+++ src/account.rs
@@ -9,6 +9,7 @@ pub enum TransactionType {
     Withdrawal,
     Dispute,
     Resolve,
+    Chargeback,
 }
 
 #[derive(Debug, Copy, Clone, Deserialize)]
@@ -20,6 +21,7 @@ pub struct TransactionRecord {
     pub tx_id: u32,
     #[serde(with = "rust_decimal::serde::float")]
     pub amount: Decimal,
+    pub disputed: bool
 }
 
 pub struct Account {
@@ -69,9 +71,20 @@ impl Account {
     }
 
     // dispute a transaction
-    fn dispute(&mut self, id: u32) { }
+    fn dispute(&mut self, id: u32) {
+        if let Some(tx) = self.transactions.get_mut(&id) {
+            if !tx.disputed {
+                tx.disputed = true;
+                self.available -= tx.amount;
+                self.held += tx.amount;
+            }
+        }
+
+    }
 
     fn resolve(&mut self, id: u32) { }
+
+    fn chargeback(&mut self, id: u32) { }
 
     pub fn process(&mut self, transaction: &TransactionRecord) {
         match transaction.tx_type {
@@ -89,7 +102,140 @@ impl Account {
             TransactionType::Resolve => {
                 self.resolve(transaction.tx_id);
             }
+            TransactionType::Chargeback => {
+                self.chargeback(transaction.tx_id);
+            }
         }
         self.transactions.insert(transaction.tx_id, *transaction);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    use rust_decimal_macros::dec;
+
+    #[test]
+    fn process_deposits() {
+        // copilot, omg!
+        let records = vec![
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Deposit,
+                tx_id: 1,
+                amount: Decimal::new(100, 2),
+                disputed: false
+            },
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Deposit,
+                tx_id: 2,
+                amount: Decimal::new(200, 2),
+                disputed: false
+            },
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Deposit,
+                tx_id: 3,
+                amount: Decimal::new(300, 2),
+                disputed: false
+            },
+        ];
+        let mut account = Account::new(1);
+        for record in records {
+            account.process(&record)
+        }
+        assert_eq!(account.available, Decimal::new(600, 2));
+    }
+
+    #[test]
+    fn process_withdrawals() {
+        // copilot, omg!
+        let records = vec![
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Deposit,
+                tx_id: 1,
+                amount: Decimal::new(200, 2),
+                disputed: false
+            },
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Withdrawal,
+                tx_id: 2,
+                amount: Decimal::new(150, 2),
+                disputed: false
+            },
+        ];
+        let mut account = Account::new(1);
+        for record in records {
+            account.process(&record)
+        }
+        assert_eq!(account.available, Decimal::new(50, 2));
+    }
+
+    #[test]
+    fn process_prevents_overdraw() {
+        // copilot, omg!
+        let records = vec![
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Deposit,
+                tx_id: 1,
+                amount: Decimal::new(200, 2),
+                disputed: false
+            },
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Withdrawal,
+                tx_id: 2,
+                amount: Decimal::new(300, 2),
+                disputed: false
+            },
+        ];
+        let mut account = Account::new(1);
+        for record in records {
+            account.process(&record)
+        }
+        assert_eq!(account.available, Decimal::new(200, 2));
+    }
+
+    #[test]
+    fn process_disputed() {
+        // copilot, omg!
+        let records = vec![
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Deposit,
+                tx_id: 1,
+                amount: dec!(201.00),
+                disputed: false
+            },
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Deposit,
+                tx_id: 2,
+                amount: dec!(140.92),
+                disputed: false
+            },
+            TransactionRecord {
+                client_id: 1,
+                tx_type: TransactionType::Dispute,
+                tx_id: 1,
+                amount: dec!(0),
+                disputed: false
+            },
+        ];
+        let mut account = Account::new(1);
+        for record in records {
+            account.process(&record)
+        }
+        assert_eq!(account.available, dec!(140.92));
+        assert_eq!(account.held, dec!(201.00));
+    }
+
+    #[test]
+    fn process_resolve() {
+
     }
 }
